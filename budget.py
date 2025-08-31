764e4d07e0aeae58f4bb4fe988e1d4832de7f718
Eric Lee <elee061202@gmail.com>
2025-08-31 02:53:20 -0500

    Updated docstrings and changed 'amount' to 'income'

--- budget.py
+++ budget.py
@@ -1,6 +1,22 @@
-class Budget:
-    def __init__(self, amount):
-        self.amount = amount
+"""
+Calculates and displays a budget amount.
 
+Attributes:
+    amount (float): The budget amount.
+    expenses (list): List of expenses.
+"""
+class Budget:
+    """
+    Initializes the budget to a value.
+
+    Parameters:
+    amount (float): The initial budget amount.
+    """
+    def __init__(self, income):
+        self.income = income
+
+    """
+    Displays the current budget amount.
+    """
     def showAmount(self):
-        print(f"Total Budget: ${self.amount}")
+        print(f"Total Budget: ${self.income}")
